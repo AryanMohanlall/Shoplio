fcca57d9333d96d0a7ba39e9ca39143b593ee798
AryanMohanlall <u23565536@tuks.co.za>
2026-03-08 14:23:18 +0200

    Update assembly information and regenerate binaries for Shoplio.Console

--- src/Shoplio.Console/obj/Debug/net10.0/Shoplio.Console.AssemblyInfo.cs
+++ src/Shoplio.Console/obj/Debug/net10.0/Shoplio.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shoplio.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bec8a86cc10438039d650f80080fcca72a7942a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c20a5b1f6a681b97947ccf562b366a0cef07acab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shoplio.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shoplio.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
